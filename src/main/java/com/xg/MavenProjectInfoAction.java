762fbf5cacec1a6b0a7ba47fdf00bdc27405a1b9
陶刚 <1102014862@qq.com>
2024-11-11 14:17:02 +0800

    feat:添加通知测试1

--- src/main/java/com/xg/MavenProjectInfoAction.java
+++ src/main/java/com/xg/MavenProjectInfoAction.java
@@ -14,6 +14,7 @@ import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
 import java.util.List;
 
+@SuppressWarnings("DialogTitleCapitalization")
 public class MavenProjectInfoAction extends AnAction {
 
     @Override
@@ -22,28 +23,33 @@ public class MavenProjectInfoAction extends AnAction {
         if (project != null) {
             // 获取Maven项目管理器
             MavenProjectsManager manager = MavenProjectsManager.getInstance(project);
-
-            // 获取所有Maven项目并提取模块名称
-            List<MavenProject> projects = manager.getProjects();
-            JSONArray jsonArray = new JSONArray();
-            for (MavenProject mavenProject : projects) {
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put("GroupId", mavenProject.getMavenId().getGroupId());
-                jsonObject.put("ArtifactId", mavenProject.getMavenId().getArtifactId());
-                jsonObject.put("Version", mavenProject.getMavenId().getVersion());
-                jsonObject.put("MavenProject.SourcePath", mavenProject.getSources().get(0));
-                jsonObject.put("MavenProject.ResourcesPath", mavenProject.getResources().get(0).getDirectory());
-                jsonObject.put("MavenProject.DisplayName", mavenProject.getDisplayName());
-                jsonObject.put("MavenProject.Name", mavenProject.getName());
-                jsonObject.put("MavenProject.Directory", mavenProject.getDirectory());
-                jsonObject.put("MavenProject.BuildDirectory", mavenProject.getBuildDirectory());
-                jsonArray.add(jsonObject);
-            }
-
             NotificationGroupManager groupManager = NotificationGroupManager.getInstance();
-            Notification notification = groupManager.getNotificationGroup("Custom Notification Group")
-                    .createNotification(jsonArray.toJSONString(), MessageType.INFO).setTitle("工程信息");
-            Notifications.Bus.notify(notification);
+
+            if (manager != null) {
+                // 获取所有Maven项目并提取模块名称
+                List<MavenProject> projects = manager.getProjects();
+                JSONArray jsonArray = new JSONArray();
+                for (MavenProject mavenProject : projects) {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("GroupId", mavenProject.getMavenId().getGroupId());
+                    jsonObject.put("ArtifactId", mavenProject.getMavenId().getArtifactId());
+                    jsonObject.put("Version", mavenProject.getMavenId().getVersion());
+                    jsonObject.put("MavenProject.SourcePath", mavenProject.getSources().get(0));
+                    jsonObject.put("MavenProject.ResourcesPath", mavenProject.getResources().get(0).getDirectory());
+                    jsonObject.put("MavenProject.DisplayName", mavenProject.getDisplayName());
+                    jsonObject.put("MavenProject.Name", mavenProject.getName());
+                    jsonObject.put("MavenProject.Directory", mavenProject.getDirectory());
+                    jsonObject.put("MavenProject.BuildDirectory", mavenProject.getBuildDirectory());
+                    jsonArray.add(jsonObject);
+                }
+                Notification notification = groupManager.getNotificationGroup("NotificationXg")
+                        .createNotification(jsonArray.toJSONString(), MessageType.INFO).setTitle("项目模块信息");
+                Notifications.Bus.notify(notification);
+            } else {
+                Notification notification = groupManager.getNotificationGroup("NotificationXg")
+                        .createNotification("暂不支持非Maven项目", MessageType.INFO).setTitle("项目模块信息");
+                Notifications.Bus.notify(notification);
+            }
         }
     }
 }

a4b6d58b376212429598f053dfb56a2db657f058
taogang <tao_gang@outlook.com>
2024-12-21 20:53:54 +0800

    feat:提示修改

--- src/main/java/com/github/xg/action/XGAnAction.java
+++ src/main/java/com/github/xg/action/XGAnAction.java
@@ -4,8 +4,6 @@ import com.github.xg.ui.XGMainDialog;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 
 public class XGAnAction extends AnAction {

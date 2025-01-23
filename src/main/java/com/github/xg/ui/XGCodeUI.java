eb4491db391e24279221b1b4f370fa7f7865ec18
陶刚 <1102014862@qq.com>
2025-01-23 09:42:52 +0800

    feat：动态渲染15%

--- src/main/java/com/github/xg/ui/XGCodeUI.java
+++ src/main/java/com/github/xg/ui/XGCodeUI.java
@@ -248,6 +248,7 @@ public class XGCodeUI {
 
             // 创建 ExpandableTextField
             ExpandableTextField expandableTextField = new ExpandableTextField();
+            expandableTextField.setToolTipText("包名路径");
             expandableTextField.setText(xgTempItem.getPackageName());
             gbc.gridx = 1; // 列索引
             gbc.gridy = i; // 行索引
